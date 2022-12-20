4bc8e65bae2a53621b167950bcca149137758157
Dragon Lore <tate.j.foster@gmail.com>
2022-12-20 10:08:05 -0700

    starting to put together first choice page

---
+++ client/src/Areas/FirstChoice/FirstChoice.js
@@ -0,0 +1,10 @@
+import "./FirstChoice.css";
+
+export default function FirstChoice() {
+	return (
+		<div className="FirstChoiceContainer">
+			<img>Pedistal image</img>
+			<p>The Darkness surrounds you</p>
+		</div>
+	);
+}
