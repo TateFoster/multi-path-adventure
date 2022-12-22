686e107def7bf27e2bb9978a356ae1c2962f5250
Dragon Lore <tate.j.foster@gmail.com>
2022-12-21 20:11:01 -0700

    setting up choices for first choice

--- client/src/Areas/FirstChoice/FirstChoice.js
+++ client/src/Areas/FirstChoice/FirstChoice.js
@@ -5,6 +5,18 @@ export default function FirstChoice() {
 		<div className="FirstChoiceContainer">
 			<img>Pedistal image</img>
 			<p>The Darkness surrounds you</p>
+			<div className="choiceBox">
+				<p>1: Choose the path of strength</p>
+			</div>
+			<div className="choiceBox">
+				<p>2: Choose the path of flexability</p>
+			</div>
+			<div className="choiceBox">
+				<p>3: Choose the path of knowledge</p>
+			</div>
+			<div className="choiceBox">
+				<p>4: Reject all paths</p>
+			</div>
 		</div>
 	);
 }
