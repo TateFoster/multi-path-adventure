5d351d17816b089936cb1711bc6c441c53c91781
Dragon Lore <tate.j.foster@gmail.com>
2023-01-05 10:56:23 -0700

    fixed double export in Pedestal

--- client/src/components/Text/Locations/Pedestal.js
+++ client/src/components/Text/Locations/Pedestal.js
@@ -1,18 +1,14 @@
 export default function Pedestal() {
 	return (
-		<>
-			<section className="writingArea"></section>
-			<section className="choicesArea"></section>
-		</>
+		<div>
+			<section className="writingArea">
+				<P>As you awaken, all around you a dark mist swirls</P>
+			</section>
+			<section className="choicesArea">
+				<P>1: The Path of Strength</P>
+				<P>2: The Path of Dexterity</P>
+				<p>3: The Path of knowledge</p>
+			</section>
+		</div>
 	);
-}
-export default function Pedestal () {
-return (
-    <div>
-        <P>As you awaken, all around you a dark mist swirls</P>
-        <P>1: The Path of Strength</P>
-        <P>2: The Path of Dexterity</P>
-        <p>3: The Path of knowledge</p>
-    </div>
-)
 }
