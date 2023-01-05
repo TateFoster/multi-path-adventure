a129f11cad57d2df0ac012a0edec57decb093eaf
Dragon Lore <tate.j.foster@gmail.com>
2023-01-05 11:19:31 -0700

    more on Intro page

--- client/src/pages/Intro/Intro.js
+++ client/src/pages/Intro/Intro.js
@@ -1,12 +1,14 @@
 import "./Intro.css";
 
 export default function Intro() {
+	const logInOut = () => {
+		loggedIn = setLoggedIn(!loggedIn);
+	};
 	return (
 		<div className="introArea">
 			<section className="options">
 				<div className="button">Log In</div>
-				<div className="button">Log In</div>
-				<div className="button">Log In</div>
+				<div className="button">Start Game</div>
 			</section>
 		</div>
 	);
