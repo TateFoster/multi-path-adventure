a8c37dae79bd89b17b37827e19fff83fe007ee7a
Dragon Lore <tate.j.foster@gmail.com>
2023-01-05 11:07:13 -0700

    setting up Intro Page shell

---
+++ client/src/pages/Intro/Intro.js
@@ -0,0 +1,13 @@
+import "./Intro.css";
+
+export default function Intro() {
+	return (
+		<div className="introArea">
+			<section className="options">
+				<div className="button">Log In</div>
+				<div className="button">Log In</div>
+				<div className="button">Log In</div>
+			</section>
+		</div>
+	);
+}
